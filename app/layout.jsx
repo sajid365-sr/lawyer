e2aef04332d9d956eee6173fb2f90cd385540887
sajid365-sr <sajidsorker86@gmail.com>
2025-08-17 09:21:25 +0600

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SessionHeader from "@/components/SessionHeader";
+import { auth } from "@/lib/auth/auth";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionHeader />
-        {children}
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
   );
